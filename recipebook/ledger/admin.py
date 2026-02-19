abd530e67fd001668f269078985c71b0d8573f40
Dexter Jethro Enriquez <dexter.jethro.enriquez@student.ateneo.edu>
2026-02-19 18:58:53 +0800

    Created the admin panel and adjusted recipe display
    Created an admin panel for recipe with an inline admin for RecipeIngredient

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Recipe, Ingredient, RecipeIngredient
+
 
 # Register your models here.
+class RecipeIngredientInLine(admin.TabularInline):
+    model = RecipeIngredient
+
+
+class RecipeAdmins(admin.ModelAdmin):
+    inlines = [RecipeIngredientInLine,]
+    model = Recipe
+    search_fields = ('name', )
+    list_display = ('name',)
+    list_filter = ('name', )
+
+
+admin.site.register(Recipe, RecipeAdmins)
