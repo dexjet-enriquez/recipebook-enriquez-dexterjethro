41ca7e05a24e2ef2338b16d9b699e06300ab00dc
Dexter Jethro Enriquez <dexter.jethro.enriquez@student.ateneo.edu>
2026-02-19 13:37:57 +0800

    Configured models.py
    Added ingredients, recipe, and recipeingredient models with their respective fields and functions

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('ingredient-detail', args=[str(self.name)])
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'ingredient'
+        verbose_name_plural = 'ingredients'
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('recipe-detail', args=[str(self.name)])
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'recipe'
+        verbose_name_plural = 'recipes'
+
+
+class RecipeIngredient(models.Model):
+    quantity = models.IntegerField()
+    ingredient = models.ForeignKey(
+        Ingredient, on_delete=models.CASCADE, related_name="recipe")
+    recipe = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE, related_name="ingredients")
+
+    def __str__(self):
+        return '{} {}'.format(self.quantity, self.ingredient.name)
+
+    class Meta:
+        ordering = ['ingredient']
+        verbose_name = 'recipe ingredient'
+        verbose_name_plural = 'recipe ingredients'
