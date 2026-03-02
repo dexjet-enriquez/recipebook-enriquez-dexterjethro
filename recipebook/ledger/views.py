adc790f9683456970c68b8e384ce509ef54686cf
dexjet-enriquez <dexter.jethro.enriquez@student.ateneo.edu>
2026-03-02 23:31:50 +0800

    Created the custom log-in and password reset flows, and adjusted views for authorization
    Created and adjusted several html files to faciliate log-in and password reset flows, added redirect urls, and modified views to prompt users to log-in

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 from .models import Recipe
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Recipe
 
@@ -16,7 +17,7 @@ class RecipeListView(ListView):
     context_object_name = 'recipes'
 
 
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'ledger/recipe_detail.html'
     context_object_name = 'recipe'
