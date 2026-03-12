18b2fdc9df4b403e354178f5fec2faa75aeb1e75
Dexter Jethro Enriquez <dexter.jethro.enriquez@student.ateneo.edu>
2026-03-12 19:22:39 +0800

    Modified RecipeAdmin to contain RecipeImage model as inline
    Created a RecipeImageInLine for RecipeAdmin and added media folder content to .gitignore for testing

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
-from .models import Recipe, Ingredient, RecipeIngredient, Profile
+from .models import Recipe, Ingredient, RecipeIngredient, Profile, RecipeImage
 
 
 # Register your models here.
@@ -11,8 +11,12 @@ class RecipeIngredientInLine(admin.TabularInline):
     model = RecipeIngredient
 
 
-class RecipeAdmins(admin.ModelAdmin):
-    inlines = [RecipeIngredientInLine,]
+class RecipeImageInLine(admin.TabularInline):
+    model = RecipeImage
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeIngredientInLine, RecipeImageInLine]
     model = Recipe
     search_fields = ('name', )
     list_display = ('name',)
@@ -28,6 +32,6 @@ class UserAdmin(BaseUserAdmin):
     inlines = [ProfileInline,]
 
 
-admin.site.register(Recipe, RecipeAdmins)
+admin.site.register(Recipe, RecipeAdmin)
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
